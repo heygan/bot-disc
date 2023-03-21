becd3a788843ba5846fa9bd3978a8978222970fd
heygan <matheusjapa2010@hotmail.com>
2023-03-21 19:03:11 -0300

    adicionado git e dev index

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+// dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+const { TOKEN } = process.env
 
-const dotenv = require('dotenv');
-dotenv.config();
-const { TOKEN, CLIENT_ID, GUILD_ID  } = process.env;
-
-//importacao dos comandos
-const fs = require("node:fs");
-const path = require("node:path");
-
+// importação dos comandos
+const fs = require("node:fs")
+const path = require("node:path")
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection(); 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.commands = new Collection()
 
 for (const file of commandFiles){
     const filePath = path.join(commandsPath, file)
@@ -27,33 +24,37 @@ for (const file of commandFiles){
     } 
 }
 
-// login do bot 
+// Login do bot
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Pronto! Login realizado como ${c.user.tag}`)
 });
+client.login(TOKEN)
 
-client.login(TOKEN);
-
-// listener de interacoes com o bot 
-
-client.on(Events.InteractionCreate,async interaction => {
-    if(!interaction.isChatInputCommand()) return
-    const command = interaction.client.commands.get(interaction.commandName)
-    if (!command){
-        console.error("Comando nao encontrado")
-        return
-
+// Listener de interações com o bot
+client.on(Events.InteractionCreate, async interaction =>{
+    if (interaction.isStringSelectMenu()){
+        const selected = interaction.values[0]
+        if (selected == "javascript"){
+            await interaction.reply("Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript")
+        } else if (selected == "python"){
+            await interaction.reply("Documentação do Python: https://www.python.org")
+        } else if (selected == "csharp"){
+            await interaction.reply("Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/")
+        } else if (selected == "discordjs"){
+            await interaction.reply("Documentação do Discord.js: https://discordjs.guide/#before-you-begin")
+        }
     }
-
+    if (!interaction.isChatInputCommand()) return
+    const command = interaction.client.commands.get(interaction.commandName)
+    if (!command) {
+        console.error("Comando não encontrado")
+        return
+    }
     try {
         await command.execute(interaction)
-    } catch (error) {
+    } 
+    catch (error) {
         console.error(error)
-        await interaction.reply("houve um erro ao executar o comando")
+        await interaction.reply("Houve um erro ao executar esse comando!")
     }
-});
-
-
-
-
-
+})
